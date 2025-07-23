5f07267f5ee69add448534996882ff8a3ab2b235
ramakhabbaz <ramakhabbaz@gmail.com>
2025-07-23 15:59:13 -0400

    Initial commit

---
+++ src/App.tsx
@@ -0,0 +1,12 @@
+
+import Home from './pages/Home'
+
+function App() {
+  return (
+    <main className="min-h-screen bg-pink-100 p-4">
+      <Home />
+    </main>
+  )
+}
+
+export default App
---
+++ src/pages/Home.tsx
@@ -0,0 +1,12 @@
+
+export default function Home() {
+  return (
+    <section className="text-center py-20">
+      <h1 className="text-5xl font-bold text-gray-800 mb-4">Hi, I’m Rama!</h1>
+      <p className="text-2xl text-gray-700">
+        Aspiring UI/UX & Frontend Developer <br />
+        HCI Master’s Student @ Georgia Tech
+      </p>
+    </section>
+  );
+}
