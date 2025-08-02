6d8f911920b6fcd8e9dae7ede2e0f6a5accc1e58
ramakhabbaz <ramakhabbaz@gmail.com>
2025-08-02 14:06:16 -0400

    Contact page added

--- src/App.tsx
+++ src/App.tsx
@@ -3,6 +3,7 @@ import Navbar from "./components/Navbar";
 import Home from "./pages/Home";
 import Footer from './components/Footer'
 import About from './pages/About';
+import Contact from './pages/Contact';
 
 function App() {
   return (
@@ -11,6 +12,7 @@ function App() {
       <Routes>
         <Route path="/" element={<Home />} />
         <Route path="/about" element={<About />} />
+        <Route path="/contact" element={<Contact />} />
       </Routes>
       <Footer />
     </>
